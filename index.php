5abcad31c0ccddf89461d05cefb1701fbd1c4507
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-16 11:57:55 -0300

    Cria a classe Page
    Essa clase é responsável por ler o header
    e o footer do projeto, juntar eles com o
    conteúdo de cada página e exibir eles juntos
     em um único arquivo usando o raintpl.

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php 
 
 require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
---
+++ views-cache/index.844c8e8f2189655f7f135e087c235dde.rtpl.php
@@ -0,0 +1,2 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>HELLO WORLD</h1>
+<h2>DEU CERTO IRMÃO</h2>
