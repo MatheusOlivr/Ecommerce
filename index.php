17e25b6374f118f0562d73107b4180ae00c5c167
MatheusOlivr <contato.matheusoliv@gmail.com>
2021-11-17 11:06:17 -0300

    Cria a classe PageAdmin para ler o template admin
    Foi criado a classe PageAdmin que le os arquivos (header,footer,index)
    da pasta res/admin, junta eles e exibe todos eles juntos usando o RainTpl

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
@@ -13,7 +14,12 @@ $app->get('/', function() {
 	$page->setTpl("index");
 
 });
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+	$page->setTpl("index");
 
+});
 $app->run();
 
  ?>
--- views-cache/index.844c8e8f2189655f7f135e087c235dde.rtpl.php
+++
@@ -1,2 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>HELLO WORLD</h1>
-<h2>DEU CERTO IRMÃO</h2>
